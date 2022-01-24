994ab07825c4f59ca7db681edbe843d85983e00d
VivekBits2210 <viveknayak2210@gmail.com>
2022-01-24 15:38:30 +0530

    Guess UI first version

--- solver/solver.py
+++ solver/solver.py
@@ -5,15 +5,24 @@ ALGO
 - Find a guessable word from all valid guesses which optimizes sum(P(letter at pos i)) + 0.5 * sum(P letter not at pos i) amongst the candidate set. Break ties effectively.
 - Repeat until there is only 1 candidate possible and guess it.
 '''
-
 from util.log_gen import get_logger
+from solver.random_strategy import RandomStrategy
+
 logger = get_logger(__file__)
 
 class Solver:
-    def __init__(self, word, guesses):
-        self.word = word
+    def __init__(self, length, guesses, wordle):
+        self.length = length
         self.guesses = guesses
-    
-    def solve(self):
-        logger.info("Solving for word: {}".format(self.word))
+        self.wordle = wordle
+        self.clue_history = []
 
+    def solve(self):
+        while(True):
+            guess = RandomStrategy(self.length).get_guess()
+            clues = self.wordle.get_clue_bits(guess)
+            if clues is None:
+                break
+            self.clue_history.append(clues)
+            self.wordle.pretty_print_guess()
+        
