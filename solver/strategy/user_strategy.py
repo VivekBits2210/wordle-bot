d4fe3e78a3fb6d8fc1d176fd25a5012b91512bad
VivekBits2210 <viveknayak2210@gmail.com>
2022-01-24 17:15:39 +0530

    print unused letters while playing

--- solver/strategy/user_strategy.py
+++ solver/strategy/user_strategy.py
@@ -1,14 +1,20 @@
+import string
 from util.constants import WORDS
 
 
 class UserStrategy:
     def __init__(self, wordle):
         self.wordle = wordle
+        qwertyKeyboard = 'qwertyuiopasdfghjklzxcvbnm'
+        self.unused_letters = (list(qwertyKeyboard))
 
     def get_guess(self):
         guess = ""
         while True:
-            guess = input("Enter guess: ").strip().lower()
+            print(f"Unused Letters: ",end='')
+            for letter in self.unused_letters:
+                print(f"{letter.upper()}", end=' ')
+            guess = input("\nEnter guess: ").strip().lower()
             if len(self.wordle.word) != len(guess):
                 print(f"Your guess '{guess}' is not of length {len(self.wordle.word)}!")
                 continue
@@ -18,5 +24,8 @@ class UserStrategy:
             if guess not in WORDS:
                 print(f"Your guess '{guess}' is not in the dictionary")
                 continue
+            for letter in guess:
+                if letter in self.unused_letters:
+                    self.unused_letters.remove(letter)
             break
         return guess
