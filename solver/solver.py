258aacfc8934f56d6083ca16012b6c60c5acbe42
VivekBits2210 <viveknayak2210@gmail.com>
2022-01-24 15:49:39 +0530

    Moving deedy algo docstring to deedy strategy file

--- solver/solver.py
+++ solver/solver.py
@@ -1,10 +1,3 @@
-'''
-ALGO
-- Find all candidates that fit the criteria.
-- Amongst the valid candidates, compute a distribution of letters at each position.
-- Find a guessable word from all valid guesses which optimizes sum(P(letter at pos i)) + 0.5 * sum(P letter not at pos i) amongst the candidate set. Break ties effectively.
-- Repeat until there is only 1 candidate possible and guess it.
-'''
 from util.log_gen import get_logger
 from solver.random_strategy import RandomStrategy
 
